2aa661eb8a69fcb7a70eb6f384d6313e9b16f800
Nisaruj Rattanaaram <nisaruj@hotmail.com>
2020-02-03 08:11:24 +0700

    Use mocha for testing

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -16,7 +16,11 @@ export default class FastphotoApp {
     constructor() {
         const app = express();
 
-        mongoose.connect(process.env.DB_CONNECTION_URI || `mongodb://${process.env.DB_HOST}:27017/${process.env.DB_NAME}`, { useNewUrlParser: true });
+        mongoose.connect(
+            process.env.DB_CONNECTION_URI || `mongodb://${process.env.DB_HOST}:27017/${process.env.DB_NAME}`,
+            { useNewUrlParser: true, useUnifiedTopology: true },
+            () => console.log('MongoDB Error'),
+        );
 
         app.use(bodyParser.json());
         app.use(bodyParser.urlencoded({ extended: true }));
