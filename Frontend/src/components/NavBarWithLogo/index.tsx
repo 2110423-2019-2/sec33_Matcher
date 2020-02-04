1c9069ab203fa761e936a2913f983985ce3cd2e3
Strik3agle98 <can_saelo@hotmail.com>
2020-02-04 18:28:57 +0700

    added all object

--- Frontend/src/components/NavBarWithLogo/index.tsx
+++ Frontend/src/components/NavBarWithLogo/index.tsx
@@ -5,8 +5,11 @@ export default () => {
   return (
       <div className="container">
         <div>
+            <h5 className='logo'>Matcher</h5>
             <p className='home'>Home</p>
-            <p className='about'>About us</p>
+            <p className='aTask'>All Tasks</p>
+            <p className='type'>Photo Types</p>
+            <button>Sign in</button>
             <hr/>
         </div>
         
