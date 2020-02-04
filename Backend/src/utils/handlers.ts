c8be213337a208e1ebae0c08baaf69bfca438782
Nisaruj Rattanaaram <nisaruj@hotmail.com>
2020-02-04 18:27:47 +0700

    Fix error handler bug

--- Backend/src/utils/handlers.ts
+++ Backend/src/utils/handlers.ts
@@ -5,10 +5,11 @@ export const asyncHandler = (fn: any) => async (req: Request, res: Response, nex
     return Promise.resolve(fn(req, res, next)).catch(next);
 };
 
-export const errorHandler = (err: Error, req: Request, res: Response): void => {
+export const errorHandler = (err: Error, req: Request, res: Response, next: any): void => {
     if (err instanceof HttpErrors.HttpError) {
         res.status(err.statusCode).json({ ...err, type: err.constructor.name, message: err.message });
     } else {
         res.status(500).json({ statusCode: 500 });
     }
+    next();
 };
