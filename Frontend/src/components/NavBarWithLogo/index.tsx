4cf7067ecce9e6af6cf2f8c428f6794449e20d38
Strik3agle98 <can_saelo@hotmail.com>
2020-02-06 10:48:32 +0700

    Import font

--- Frontend/src/components/NavBarWithLogo/index.tsx
+++ Frontend/src/components/NavBarWithLogo/index.tsx
@@ -3,16 +3,15 @@ import "./index.css";
 
 export default () => {
   return (
-      <div className="container">
-        <div>
-            <h5 className='logo'>Matcher</h5>
-            <p className='home'>Home</p>
-            <p className='aTask'>All Tasks</p>
-            <p className='type'>Photo Types</p>
-            <button>Sign in</button>
-            <hr/>
-        </div>
-        
+    <div className="container">
+      <div>
+        <h5 className="logo">Matcher</h5>
+        <p className="home">Home</p>
+        <p className="aTask">All Tasks</p>
+        <p className="type">Photo Types</p>
+        <button>Sign in</button>
+        <hr />
       </div>
+    </div>
   );
 };
