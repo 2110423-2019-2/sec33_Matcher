8fbbb706559a74c9c141e9ddbf2268584e537c6b
phumnoi1 <phumnoi1@gmail.com>
2020-02-15 17:03:38 +0700

    no message

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Home } from "./pages";
+import { NavBar, ComponentList } from "./components"
 import "./index.css";
 const App: React.FC = () => {
   return (
     <div>
+      <NavBar isLogin = {true} username = 'John Doe'/>
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
