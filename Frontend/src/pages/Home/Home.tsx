09ff809fc2c53c03d256538533601f657d2a8b65
phumnoi1 <phumnoi1@gmail.com>
2020-02-15 17:29:03 +0700

    no message

--- Frontend/src/pages/Home/Home.tsx
+++ Frontend/src/pages/Home/Home.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import "./index.css";
+import { Button, VerticalCard, TaskCard } from "../../components/index";
+import { ReactComponent as Chevron } from '../../assets/icons/chevron-right.svg';
 export default () => {
   return (
     <div className="home">
-      <div style={{ height: "10vh" }}>navbar</div>
       <div className="home-sub-title">
         <div className="line-before-home-sub-title" />
         <h5>A picture is worth a thousand words</h5>
@@ -12,6 +13,17 @@ export default () => {
         <h1>Memorize your scene with quality shots ...</h1>
       </div>
       <div className="home-button-line"></div>
+
+      <div className="col-4">
+            <Button type="filled">Find jobs</Button>
+            
+            <Button type="outlined">Create task</Button>
+            <br /><br />
+        </div>
+
+
     </div>
+    
   );
 };
+
