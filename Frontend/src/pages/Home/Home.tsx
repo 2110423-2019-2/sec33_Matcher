9eabcb9852420afbb9ff75f0e1de887b2245751a
phumnoi1 <phumnoi1@gmail.com>
2020-02-15 18:30:11 +0700

    no message

--- Frontend/src/pages/Home/Home.tsx
+++ Frontend/src/pages/Home/Home.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./index.css";
 import { Button, VerticalCard, TaskCard } from "../../components/index";
 import { ReactComponent as Chevron } from '../../assets/icons/chevron-right.svg';
-import { ReactComponent as camera } from '../../assets/camera.svg';
+import camera from '../../assets/camera.svg';
 export default () => {
   return (
     <div className="home">
@@ -11,19 +11,22 @@ export default () => {
         <h5>A picture is worth a thousand words</h5>
       </div>
       <div className="home-title">
-        <h1>Memorize your scene with quality shots ...</h1>
+        <h1>Memorize your scene <br></br>with quality shots ...</h1>
       </div>
-      <div className="home-button-line"></div>
-
-      <div className="col-4">
-            <Button type="filled">Find jobs</Button>
-            
-            <Button type="outlined">Create task</Button>
-            <br /><br />
+      
+        <div className="pic">
+        <img src={camera}/>
         </div>
+        <br></br>
 
-
-    </div>
+      <div className="home-button-line"> 
+      <Button type="filled">Find jobs</Button>
+        <Button type="outlined">Create task</Button> 
+        <br /><br />
+      </div>
+        
+       
+      </div>
     
   );
 };
