31bfcb00038e7fc72d2a8de7ca9617a4e9ecf57a
Strik3agle98 <can_saelo@hotmail.com>
2020-02-19 14:07:28 +0700

    progress on Register page layout

--- Frontend/src/pages/register/index.tsx
+++ Frontend/src/pages/register/index.tsx
@@ -1,23 +1,52 @@
 import React from "react";
 import "./index.scss";
+import Checkbox, { CheckboxProps } from '@material-ui/core/Checkbox';
 import { ReactComponent as RegisterBackground } from "../../assets/creating-photo.svg";
-import { NavBar, Footer } from "../../components";
+import { NavBar, Footer, Input, Button } from "../../components";
 
+// const [checkedPhotographer, setCheckedPhotographer] = React.useState(false);
+
+// const handleCheck = (field: boolean) => {
+
+// }
 
 export default () => {
-    return (
-      <div className="row center registerPage">
-        <NavBar isLogin = {false} username = 'John Doe'/>
-        <div className="col-6">
-          <RegisterBackground className="registerBackground" />
-        </div>
-        <div className="col-6">
-          <div className="row center">
-          {/* <Input label="Firstname" variant="filled" /> */}
-          </div>
-        </div>
-        {/* <Footer/> */}
+  return (
+    <div className="row registerPage">
+      {/* <NavBar isLogin = {false} username = 'John Doe'/> */}
+      <div className="col-6">
+        <RegisterBackground className="registerBackground" />
       </div>
-    );
-  };
-  
+      <div className="col-1" />
+      <div className="col-4">
+        <form>
+          <div className="row">
+            <div className="col-8">
+              <h1 className="signInHeader">SIGN IN</h1>
+              <Input label="Firstname" variant="filled" fullWidth />
+              <Input label="Lastname" variant="filled" fullWidth />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-12">
+              <Input label="Email" variant="filled" fullWidth />
+              <Input label="Password" variant="filled" fullWidth />
+              <Input label="Re-enter password" variant="filled" fullWidth />
+          
+              <div className="col-2">
+                  <Checkbox
+                    // checked={checkedPhotographer}
+                    // onChange={handleCheck("checkedPhotographer")}
+                    value="checkedPhotographer"
+                  />
+              </div>
+                  <p>asdf</p>
+             
+            </div>
+          </div>
+        </form>
+      </div>
+      {/* <Footer/> */}
+    </div>
+  );
+};
