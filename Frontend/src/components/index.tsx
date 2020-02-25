8f95b5b72fe5fb9ef655c45e220eb907784379a4
zpoom <poom007.sk135@gmail.com>
2020-02-25 10:02:18 +0700

    add script for lint and format

--- Frontend/src/components/index.tsx
+++ Frontend/src/components/index.tsx
@@ -1,17 +1,9 @@
-import NavBar from './NavBar'
-import UserBar from './UserBar'
+import NavBar from './NavBar';
+import UserBar from './UserBar';
 import ComponentList from './ComponentList';
 import Button from './Button';
 import VerticalCard from './VerticalCard';
 import TaskCard from './TaskCard';
 import Input from './Input';
 
-export {
-    NavBar,
-    UserBar,
-    ComponentList,
-    Button,
-    VerticalCard,
-    TaskCard,
-    Input,
-}
+export { NavBar, UserBar, ComponentList, Button, VerticalCard, TaskCard, Input };
