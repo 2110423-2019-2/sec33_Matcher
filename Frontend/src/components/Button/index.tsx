ba0fe3de6c40723f29ed6f56bd08a407bad76227
Nisaruj Rattanaaram <nisaruj@hotmail.com>
2020-02-27 09:04:18 +0700

    Refactor homepage

--- Frontend/src/components/Button/index.tsx
+++ Frontend/src/components/Button/index.tsx
@@ -4,9 +4,10 @@ import "./index.scss";
 interface ButtonProps {
     children?: React.ReactNode
     type?: "filled" | "outlined"
+    className?: string
 }
 
-export default ({ children, type = 'filled' }: ButtonProps) => 
-    <div className={`button ${type}`}>
+export default ({ children, type = 'filled', className = '' }: ButtonProps) => 
+    <div className={`button ${type} ${className}`}>
         { children }
     </div>
--- Frontend/src/pages/Home/Home.tsx
+++ Frontend/src/pages/Home/Home.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./index.css";
+import "./index.scss";
 import { Button, VerticalCard, TaskCard } from "../../components/index";
 import { ReactComponent as Chevron } from '../../assets/icons/chevron-right.svg';
 import camera from '../../assets/camera.svg';
@@ -10,6 +10,39 @@ import event from '../../assets/vcard/event.svg'
 import product from '../../assets/vcard/product.svg'
 import place from '../../assets/vcard/place.svg'
 import cafe from '../../assets/vcard/cafe.svg'
+
+const categoryCards = [
+  {
+    src: graduation,
+    alt: 'Graduation',
+    text: 'Graduation'
+  },
+  {
+    src: wedding,
+    alt: 'Wedding',
+    text: 'Wedding'
+  },
+  {
+    src: event,
+    alt: 'Event',
+    text: 'Event'
+  },
+  {
+    src: product,
+    alt: 'Product',
+    text: 'Product'
+  },
+  {
+    src: place,
+    alt: 'Place',
+    text: 'Place'
+  },
+  {
+    src: cafe,
+    alt: 'Café & Restaurant',
+    text: 'Café & Restaurant'
+  },
+]
 
 
 export default () => {
@@ -23,76 +56,73 @@ export default () => {
         <div className="home-title">
           <h1>Memorize your scene <br></br>with quality shots ...</h1>
         </div>
-        
+
         <div className="pic">
-          <img src={camera}/>
+          <img src={camera} alt="camera" />
         </div>
         <br></br>
 
-        <div className="home-button-line"> 
+        <div className="home-button-line">
           <Button type="filled">Find jobs</Button>
-          <Button type="outlined">Create task</Button> 
+          <Button type="outlined" className="createTaskBtn">Create task</Button>
         </div>
       </div>
 
-        <div className="seeBlock">
-          <h5>see all task</h5>
-        </div>
-        
-        <div className="matcherBlock">
-          <div className="matcherletter"><h5>© Matcher inc.</h5></div>
-          <div className="socialicon"><img src={social}/></div>
-        </div>
-        <br></br>
-        
-        <div className="blank"></div>
+      <div className="seeBlock">
+        <h5>see all task</h5>
+      </div>
+
+      <div className="matcherBlock">
+        <div className="matcherletter"><h5>© Matcher inc.</h5></div>
+        <div className="socialicon"><img src={social} alt="social icon" /></div>
+      </div>
+      <br></br>
+
+      <div className="blank"></div>
 
       <div className="pad">
-        <div className="jobby">
-        <h5>Jobs near by you</h5>
-        </div>
-
-        <div className="seeall">
-        <h5>see all <Chevron /> </h5>
-        </div>
-
-        <div className="blank2"></div>
-          
-        <div className="task">
-              <TaskCard thumbnail="https://picsum.photos/200/300" name="John Doe" location="Siam Paragon" price={300}/>
-              <TaskCard thumbnail="https://picsum.photos/200/300" name="John Doe" location="Siam Paragon" price={300}/>
-              <TaskCard thumbnail="https://picsum.photos/200/300" name="John Doe" location="Siam Paragon" price={300}/>
-              <TaskCard thumbnail="https://picsum.photos/200/300" name="John Doe" location="Siam Paragon" price={300}/>
-              <TaskCard thumbnail="https://picsum.photos/200/300" name="John Doe" location="Siam Paragon" price={300}/>
-              <TaskCard thumbnail="https://picsum.photos/200/300" name="John Doe" location="Siam Paragon" price={300}/>
+        <div className="row">
+          <div className="col-6 left subHeader">
+            <h5>Jobs near by you</h5>
           </div>
-
-        <div className="jobby">
-        <h5>Photo types</h5>
+          <div className="col-6 right subHeader seeall">
+            <h5>see all <Chevron /></h5>
+          </div>
         </div>
 
-        <div className="vcard">
-          <div className="vfloat3"><VerticalCard src={graduation} alt="Graduation" text="Graduation" /></div>
-          <div className="vfloat2"><VerticalCard src={wedding} alt="Wedding" text="Wedding" /></div>
-          <div className="vfloat1"><VerticalCard src={event} alt="Event" text="Event" /></div>
-          <div className="vfloat3"><VerticalCard src={product} alt="Product" text="Product" /></div>
-          <div className="vfloat2"><VerticalCard src={place} alt="Place" text="Place" /></div>
-          <div className="vfloat1"><VerticalCard src={cafe} alt="Café & Restaurant" text="Café & Restaurant" /></div>
+        <div className="row task">
+          <TaskCard thumbnail="https://picsum.photos/200/300" name="John Doe" location="Siam Paragon" price={300} />
+          <TaskCard thumbnail="https://picsum.photos/200/300" name="John Doe" location="Siam Paragon" price={300} />
+          <TaskCard thumbnail="https://picsum.photos/200/300" name="John Doe" location="Siam Paragon" price={300} />
+          <TaskCard thumbnail="https://picsum.photos/200/300" name="John Doe" location="Siam Paragon" price={300} />
+          <TaskCard thumbnail="https://picsum.photos/200/300" name="John Doe" location="Siam Paragon" price={300} />
+          <TaskCard thumbnail="https://picsum.photos/200/300" name="John Doe" location="Siam Paragon" price={300} />
+        </div>
+
+        <div className="photoTypes subHeader">
+          <h5>Photo types</h5>
+          <div className="row center">
+            {
+              categoryCards.slice(0, 3).map(({ src, alt, text }, index) => (
+                <div className="col-4" style={{ marginTop: 50 * (2 - index) }} >
+                  <VerticalCard src={src} alt={alt} text={text} />
+                </div>
+              ))
+            }
+          </div>
+          <div className="row center">
+            {
+              categoryCards.slice(3, 6).map(({ src, alt, text }, index) => (
+                <div className="col-4" style={{ marginTop: 50 * (2 - index) }}>
+                  <VerticalCard src={src} alt={alt} text={text} />
+                </div>
+              ))
+            }
+          </div>
         </div>
       </div>
-
-        <div className="matcherBlock2">
-          <div className="matcherletter"><h5>© Matcher inc.</h5></div>
-          <div className="socialicon"><img src={social}/></div>
-        </div>
-
-        
-        
-    
-
     </div>
 
-    
   );
 };
 
