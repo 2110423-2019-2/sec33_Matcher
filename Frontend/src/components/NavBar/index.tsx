7751e484a97128a2f9b45949abe7390bfd949b34
Strik3agle98 <can_saelo@hotmail.com>
2020-03-10 22:26:12 +0700

    update NavBar CSS, add routing

--- Frontend/src/components/NavBar/index.tsx
+++ Frontend/src/components/NavBar/index.tsx
@@ -2,23 +2,33 @@ import React, { useContext } from "react";
 import "./index.css";
 import { UserBar, Button } from "../";
 import { AuthContext } from "../../context/AuthContext";
+import { Link } from "react-router-dom";
 
 export default (props: any) => {
   const { auth } = useContext(AuthContext);
   return (
     <div className="navBar">
-      <h3 className="logo">matcher</h3>
-      <h6 className="navHome navBarItem">Home</h6>
+      <Link to='/index'>
+        <h3 className="logo">matcher</h3>
+        <h6 className="navHome navBarItem">Home</h6>
+      </Link>
       <h6 className="aTask navBarItem">All Tasks</h6>
       <h6 className="type navBarItem">Photo Types</h6>
       <div className="NavBarUser">
         {auth.isLogin ? (
           <UserBar username={auth.username} />
         ) : (
-          <Button type="outlined">Sign In</Button>
+          <Link to='/signin'>
+            <Button type="outlined">Sign In</Button>
+          </Link>
         )}
       </div>
-      <hr className="slider" />
+      {auth.isLogin ? (
+          <hr className="slider" />
+        ) : (
+          <hr className="sliderB" />
+        )}
+      
     </div>
   );
 };
