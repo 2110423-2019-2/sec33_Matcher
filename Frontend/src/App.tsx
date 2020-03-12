3266241e88008aebc29b6b4812489dc30ffb487a
Siwat_Keng <siwat.keng.pongpanit@gmail.com>
2020-03-12 17:47:22 +0700

    add(some) handle change/submit

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -14,7 +14,7 @@ const App: React.FC = () => {
   return (
     <AuthContextProvider value={{ auth, authDispatcher }}>
       <div>
-        <NavBar username="John Doe" />
+        <NavBar username={"John Doe"} isLogin={true} />
         {/* <Footer/> */}
         {/* <ComponentList /> */}
         <EditProfile />
--- Frontend/src/pages/EditProfile/index.tsx
+++ Frontend/src/pages/EditProfile/index.tsx
@@ -1,55 +1,74 @@
-import React, { useContext } from "react";
-import { AuthContext } from "../../context/AuthContext";
+import React, { useContext, useState } from "react";
 import "./index.scss";
 import { Input, Button } from "../../components";
 import { ReactComponent as Chevron } from "../../assets/icons/chevron-right.svg";
+import { Link, useHistory } from 'react-router-dom';
+import { AuthContext } from "../../context/AuthContext";
+
 export default () => {
+
   const { auth } = useContext(AuthContext);
+  
+  const [userInfo, setUserInfo] = useState({
+    name: '',
+    surname: '',
+    password: '',
+    re_password: '',
+    email: '',
+});
+  // const history = useHistory();
+
+  const handleChange = (field: string) => (e: any) => {
+    setUserInfo({
+      ...userInfo,
+      [field]: e.target.value
+    })
+  }
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    console.log('done!')
+};  
+
   return (
     <div className="editProfilePage">
-      <div className="row editProfileTitle">
-        <div className="col-7 helloMessage">
-          <h5>{`Good morning, ${auth.username}`}</h5>
-        </div>
-        <div className="col lineAfterHello" />
-        <div className="row editProfileMenu">
-          <div className="col-4">
-            <p>Edit Profile</p>
-          </div>
-          <div className="col-4">
-            <p>Your tasks</p>
-          </div>
-        </div>
-      </div>
-      <div className="row">
-        <p>You can edit your Profile here. Click save changes after that.</p>
-      </div>
-      <div className="row editProfileForm">
-        <div className="col-6">
-          <Input variant="filled" defaultValue="test" fullWidth />
-        </div>
-        <div className="col-6">
-          <Input variant="filled" defaultValue="test" fullWidth />
-        </div>
-      </div>
-      <div className="row editProfileForm">
-        <div className="col-6">
-          <Input variant="filled" defaultValue="test" fullWidth />
-        </div>
-        <div className="col-6">
-          <Input variant="filled" defaultValue="test" fullWidth />
-        </div>
-      </div>
-      <div className="row editProfileForm">
-        <div className="col-6">
-          <Input variant="filled" defaultValue="test" fullWidth />
-        </div>
-        <div className="col-6">
-          <Button type="filled" fullWidth>
-            Save Changes <Chevron style={{ strokeWidth: 1 }} />
-          </Button>
-        </div>
+    <div className="row editProfileTitle">
+      <div className="col-3">
+          <h4 className="editProfileHeader">Create Task</h4>
       </div>
     </div>
+    <div className="row messageProfile">
+      <div className="col-8">
+        <p>You can edit your profile here. Click 'Save changes' after that.</p>
+      </div>
+    </div>
+    <form onSubmit={handleSubmit}>
+    <div className="row editProfileTitle">
+      <div className="col-6">
+        <Input variant="filled" onChange={handleChange('name')} label="Name" defaultValue={auth.username} fullWidth />
+      </div>
+      <div className="col-6">
+        <Input variant="filled" onChange={handleChange('surname')} label="Surname" defaultValue={auth.username} fullWidth />
+      </div>
+    </div>
+    <div className="row editProfileTitle">
+      <div className="col-6">
+        <Input variant="filled" onChange={handleChange('password')} type='password' label="Password" fullWidth />
+      </div>
+      <div className="col-6">
+        <Input variant="filled" onChange={handleChange('re_password')} type='password' label="Re-enter password" fullWidth />
+      </div>
+    </div>
+    <div className="row editProfileTitle">
+      <div className="col-6">
+        <Input variant="filled" onChange={handleChange('email')} label="Email" defaultValue={auth.username} fullWidth />
+      </div>
+      <div className="col-6">
+        <Button type="invert" fullWidth>Save Changes<Chevron style={{ strokeWidth: 1 }} />
+        </Button>
+      </div>
+    </div>
+    </form>
+  </div>
   );
-};
+}; 
