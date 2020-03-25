55545d346ccc7c50ac261a7e6e9e99d52fa10183
zpoom <poom007.sk135@gmail.com>
2020-03-25 14:51:12 +0700

    add fetchTasks

--- Frontend/src/components/UserTasks/index.tsx
+++ Frontend/src/components/UserTasks/index.tsx
@@ -1,19 +1,51 @@
-import React, { useContext } from 'react';
-import { Section } from '../'
+import React, { useContext, useEffect, useState } from 'react';
+import { Section, TaskCard } from '../'
 import './index.scss';
 import { AuthContext } from '../../context/AuthContext';
+import { getAvailableTasks, getMatchedTasks, getFinishedTasks } from '../../api/task';
+import { dummyTasks } from '../../const';
+interface ITask {
+    username: string,
+    location: string,
+    img: string,
+    price: number
+}
 export default () => {
     const { auth } = useContext(AuthContext);
+    const [availableTasks, setAvailableTasks] = useState<Array<ITask>>([]);
+    const [matchedTasks, setMatchedTasks] = useState<Array<ITask>>([]);
+    const [finishedTasks, setFinishedTasks] = useState<Array<ITask>>([]);
+    const fetchTasks = () => {
+        getAvailableTasks().then(tasks => {
+            setAvailableTasks(tasks);
+        }).catch(err =>
+            console.log(err)
+        );
+        getMatchedTasks().then(tasks => {
+            setMatchedTasks(tasks);
+        }).catch(err =>
+            console.log(err)
+        );
+        getFinishedTasks().then(tasks => {
+            setFinishedTasks(tasks);
+        }).catch(err =>
+            console.log(err)
+        );
+    }
+
+    useEffect(() => {
+        fetchTasks();
+    }, [])
     return (
         <div className="sectionContainer">
             <Section title={`${auth.role === 'customer' ? 'Available' : 'Pending'} Task`}>
-                {/* {pendingTasks.map(task => <TaskCard name={task.username} location={task.location} profilePic={task.img} price={task.price} thumbnail={task.img} />)} */}
+                {dummyTasks.length === 0 ? null : dummyTasks.map(task => <TaskCard name={task.username} location={task.location} profilePic={task.img} price={task.price} thumbnail={task.img} button={auth.role === 'customer' ? 'Edit' : 'Pending'} />)}
             </Section>
             <Section title="Matched Task">
-                {/* <TaskCard></TaskCard> */}
+                {/* {matchedTasks.map(task => <TaskCard name={task.username} location={task.location} profilePic={task.img} price={task.price} thumbnail={task.img} />)} */}
             </Section>
             <Section title="Past Task">
-                {/* <TaskCard></TaskCard> */}
+                {/* {finishedTasks.map(task => <TaskCard name={task.username} location={task.location} profilePic={task.img} price={task.price} thumbnail={task.img} />)} */}
             </Section>
         </div>
     )
