6b5954cd5de70aa1c0ff113dbfe693ec0c6c023b
pawat <p.amonpitakpun@gmail.com>
2020-03-31 21:18:08 +0700

    delete description and available time field

--- Backend/src/models/task.model.ts
+++ Backend/src/models/task.model.ts
@@ -3,10 +3,8 @@ import { IUser } from '../models';
 
 const TaskSchema = new Schema({
     title: { type: String, required: [true, 'title can not be blank'] },
-    description: { type: String },
     location: { type: String, required: true },
     owner: { type: Schema.Types.ObjectId, ref: 'User', required: [true, 'owner is required'] },
-    availableTime: { type: Date, required: [true, 'available time is required'] },
     photoStyle: { type: String, required: [true, 'photo style is required'] },
     price: { type: Number, required: [true, 'the minimum price is required'] },
     image: { type: String },
@@ -19,10 +17,8 @@ const TaskSchema = new Schema({
 
 export interface ITask extends Document {
     title: string;
-    description: string;
     location: string;
     owner: IUser['_id'];
-    availableTime: Date;
     photoStyle: string;
     price: number;
     image: string;
