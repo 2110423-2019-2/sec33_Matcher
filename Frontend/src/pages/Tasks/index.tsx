3348d6249cfe80a9e20dcfcb02150f448c29c37a
Strik3agle98 <can_saelo@hotmail.com>
2020-04-03 17:35:31 +0700

    Filter Dialog Layout

--- Frontend/src/pages/Tasks/index.tsx
+++ Frontend/src/pages/Tasks/index.tsx
@@ -1,12 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import './index.scss';
 import { PHOTO_CATEGORIES, dummyTasks } from '../../const';
-import { TaskCard } from '../../components';
+import { TaskCard, Input, Button } from '../../components';
 import { getAvailableTasks, matchTask } from '../../api/task';
+import { DialogContent, DialogContentText, DialogActions, Dialog, DialogTitle, Slider } from '@material-ui/core';
 export default () => {
+    const [dialog, setDialog] = useState(false);
     const [tasks, setTasks] = useState<Array<any>>([]);
+    const [dialogData, setDialogData] = useState({
+        location: '',
+        priceRange: [0, 20000]
+    })
     const [taskFilter, setTaskFilter] = useState({
-        filter: '',
+        photoStyle: '',
+        location: '',
+        priceRange: [0, 20000]
     });
     useEffect(() => {
         getAvailableTasks()
@@ -16,18 +24,54 @@ export default () => {
             })
             .catch(err => console.log(err));
     }, []);
-    const filteredTasks = tasks.filter(task => task.photoStyle === taskFilter.filter || taskFilter.filter === '');
+    const filteredTasks = tasks.filter(
+        task =>
+            task.photoStyle === taskFilter.photoStyle ||
+            (taskFilter.photoStyle === '' &&
+                task.location.includes(taskFilter.location) &&
+                task.price >= taskFilter.priceRange[0] &&
+                task.price <= taskFilter.priceRange[1]),
+    );
 
     const onclick = (category: string) => (e: any) => {
-        if (taskFilter.filter === category) {
+        if (taskFilter.photoStyle === category) {
             setTaskFilter({
-                filter: '',
+                ...taskFilter,
+                photoStyle: '',
             });
         } else {
             setTaskFilter({
-                filter: category,
+                ...taskFilter,
+                photoStyle: category,
             });
         }
+    };
+
+    const closeDialog = () => {
+        setDialog(false);
+    };
+
+    const handleChange = (field: string) => (e:any) => {
+        setDialogData({
+            ...dialogData,
+            [field]: e.target.value
+        })
+    };
+
+    const handleSlider = (e:any, Val:number | number[]) => {
+        setDialogData({
+            ...dialogData,
+            priceRange: Val as number[]
+        })
+    }
+
+    const onFilter = () => {
+        setTaskFilter({
+            ...taskFilter,
+            location: dialogData.location,
+            priceRange: dialogData.priceRange,
+        });
+        setDialog(false);
     };
 
     const onAccept = (id: string) => (e: any) => {
@@ -45,7 +89,7 @@ export default () => {
                         return (
                             <p
                                 onClick={onclick(category)}
-                                className={`${category === taskFilter.filter ? 'activeFilter' : ''} pButton`}
+                                className={`${category === taskFilter.photoStyle ? 'activeFilter' : ''} pButton`}
                             >
                                 {category}
                             </p>
@@ -53,7 +97,14 @@ export default () => {
                     })}
                 </div>
                 <div className="sortBy">
-                    <p>sort by</p>
+                    <p
+                        onClick={() => {
+                            setDialog(true);
+                        }}
+                        className="pButton"
+                    >
+                        more filter
+                    </p>
                 </div>
             </div>
             <div className="row tasks">
@@ -72,6 +123,52 @@ export default () => {
                     );
                 })}
             </div>
+            <Dialog open={dialog} onClose={closeDialog} fullWidth={true}>
+                <DialogTitle className="dialogTitle">Additional Filter</DialogTitle>
+                <DialogContent>
+                    <div className="locationIn">
+                        <Input variant="filled" onChange={handleChange('location')} label="Location" fullWidth />
+                    </div>
+                    <h6 className="dialogLabel">Price</h6>
+                    <div className="row">
+                        <div className="col-3">
+                            <Input 
+                                value={dialogData.priceRange[0]}
+                                label="min"
+                                variant="filled"
+                                onChange={handleChange('lowerPrice')} 
+                                type="number"
+                            />
+                        </div>
+                        <div className="col-6">
+                            <Slider
+                                value={dialogData.priceRange}
+                                min={0}
+                                max={20000}
+                                scale={(x) => x ** 2}
+                                onChange={handleSlider}
+                            />
+                        </div>
+                        <div className="col-3">
+                            <Input 
+                                value={dialogData.priceRange[1]}
+                                label="max"
+                                variant="filled"
+                                onChange={handleChange('upperPrice')} 
+                                type="number"
+                            />
+                        </div>
+                    </div>
+                </DialogContent>
+                <DialogActions>
+                    <Button type="outlined" onClick={closeDialog}>
+                        Cancel
+                    </Button>
+                    <Button onClick={onFilter}>
+                        Apply Filter
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </div>
     );
 };
