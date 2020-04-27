9fc25a0463e005a1d04ec68f2dca3b79434e3745
Bank Biz <thaworn_sgt280@hotmail.com>
2020-04-27 23:07:08 +0700

    Update index.tsx

--- Frontend/src/pages/Home/index.tsx
+++ Frontend/src/pages/Home/index.tsx
@@ -62,7 +62,7 @@ export default () => {
                         <Button type="filled" className="homeBtn">Find jobs</Button>
                     </Link>
                     <Link to="/console?tab=create">
-                        <Button type="outlined" className="createTaskBtn">
+                        <Button type="outlined" className="homeBtn">
                             Create task
                         </Button>
                     </Link>
